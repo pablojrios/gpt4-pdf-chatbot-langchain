fac3e664be48f4210fa015b0bdb24abe51b83bb5
riosp <riosp@avaya.com>
2023-06-20 12:11:21 -0300

    Use LLM model gpt-3.5-turbo-0301 and change the number of documents to retrieve from 4 (default) to 2.

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -21,12 +21,13 @@ Helpful answer in markdown:`;
 export const makeChain = (vectorstore: PineconeStore) => {
   const model = new OpenAI({
     temperature: 0, // increase temepreature to get more creative answers
-    modelName: 'gpt-3.5-turbo', //change this to gpt-4 if you have access
+    // modelName: 'gpt-3.5-turbo', //change this to gpt-4 if you have access
+    modelName: 'gpt-3.5-turbo-0301' // https://platform.openai.com/account/rate-limits
   });
 
   const chain = ConversationalRetrievalQAChain.fromLLM(
     model,
-    vectorstore.asRetriever(),
+    vectorstore.asRetriever(2),
     {
       qaTemplate: QA_PROMPT,
       questionGeneratorTemplate: CONDENSE_PROMPT,
